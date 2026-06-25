4689e16c3f44f09326c7262db8785ef7988b3fd7
Eiji-16 <ajph305@gmail.com>
2026-06-25 17:47:23 +0800

    up

--- resources/js/Login.jsx
+++ resources/js/Login.jsx
@@ -5,39 +5,56 @@ const samplepic = "/image/sample.jpg";
 function Login({ onLogin }) {
     return (
         <div className="Login-box">
-
-                                {/* Design side panel */}
             <div className="Design-side">
-                <img src={samplepic} alt="Design Picture"/>
+        
+                <div className="Design-overlay">
+                    
+                </div>
             </div>
 
-                                {/* Login side panel */}
             <div className="Login-side">
-
-            <div className="WelcomeText"> <h1><b>Welcome</b></h1> </div>
-
-                <div className="input-group">
-                    <div className="input-icon">
-                        <i className="fa-solid fa-user"></i>
-                        <label htmlFor="uname"><b>User Account</b></label>
-                        <input type="text" className="input-field"  name="uname" required />
-                    </div>
+                <div className="WelcomeText">
+                    <h2><b>Welcome back</b></h2>
+                    <p>Sign in to your account to continue.</p>
                 </div>
-                <div className="input-group">
-                    <div className="input-icon">
-                        <i className="fa-solid fa-lock"></i>
-                        <label htmlFor="psw"><b>Password</b></label>
-                        <input type="password" className="input-field"  name="psw" required />
+
+                <form className="Login-form" onSubmit={onLogin}>
+                    <div className="input-group">
+                        <label htmlFor="uname">Username</label>
+                        <div className="input-icon">
+                            <i className="fa-solid fa-user"></i>
+                            <input
+                                id="uname"
+                                type="text"
+                                className="input-field"
+                                name="uname"
+                                placeholder="Enter your username"
+                                required
+                            />
+                        </div>
                     </div>
-                </div>
-                                {/* Forgot password */}
-                    <div className = "ForgetPassword">Forgot your password?</div>
-                                {/* Log in Button */}
-                    <button type="submit" className="Login-button" onClick={onLogin}>
+
+                    <div className="input-group">
+                        <label htmlFor="psw">Password</label>
+                        <div className="input-icon">
+                            <i className="fa-solid fa-lock"></i>
+                            <input
+                                id="psw"
+                                type="password"
+                                className="input-field"
+                                name="psw"
+                                placeholder="Enter your password"
+                                required
+                            />
+                        </div>
+                    </div>
+
+                    <div className="ForgetPassword">Forgot your password?</div>
+
+                    <button type="submit" className="Login-button">
                         <b>Login</b>
                     </button>
-
-
+                </form>
             </div>
         </div>
     );
