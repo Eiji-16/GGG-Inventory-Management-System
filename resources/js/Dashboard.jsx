42ff1c2f9a204ec5b609b018e4967256348ae36b
Eiji-16 <ajph305@gmail.com>
2026-06-27 15:13:09 +0800

    up

--- resources/js/Dashboard.jsx
+++ resources/js/Dashboard.jsx
@@ -5,22 +5,52 @@ import {
 } from 'recharts';
 import '../css/Dashboard.css';
 
-function Dashboard() {
-  return (
-    <>
-      <header>
-        <nav className="navbar">
-          {/* Navbar contents */}
-        </nav>
-      </header>
 
-      <aside className="sidebar-menu">
-        {/* Sidebar contents */}
-      </aside>
-    </>
+// Dashboard.jsx
+function Dashboard() {
+  
+  return (
+    
+      <div className = "dashboard-container">
+
+        
+  
+        <aside className = "sidebar">
+
+          <div className="sidebar-brands">
+            <img src="/image/sample.jpg" alt="GGG Logo" className="sidebar-logo-img" />
+          </div>
+
+          
+
+          <ul className="sidebar-menu">
+            <li className="sidebar-item">
+              <a href="#dashboard"><img src="/image/Icons/dashboard.png" alt="" className="sidebar-icon" />Dashboard</a>
+            </li>
+            <li className="sidebar-item">
+              <a href="#products"><img src="/image/Icons/supplier.png" alt="" className="sidebar-icon" />Product & Supplier Management</a>
+            </li>
+            <li className="sidebar-item">
+              <a href="#stock"><img src="/image/Icons/box.png" alt="" className="sidebar-icon" />Stock Control</a>
+            </li>
+            <li className="sidebar-item">
+              <a href="#eoq"><img src="/image/Icons/calculator.png" alt="" className="sidebar-icon" />EOQ-Calculator</a>
+            </li>
+            <li className="sidebar-item">
+              <a href="#forecasting"><img src="/image/Icons/forecast-analytics.png" alt="" className="sidebar-icon" /> Demand Forecasting</a>
+            </li>
+            <li className="sidebar-item">
+              <a href="#reports"><img src="/image/Icons/report.png" alt="" className="sidebar-icon" />Report & Analytics</a>
+            </li>
+          </ul>
+        </aside>
+      
+        <main className = "overview">
+          
+        </main>
+      </div>
+    
   );
 }
-
-
 
 export default Dashboard;
