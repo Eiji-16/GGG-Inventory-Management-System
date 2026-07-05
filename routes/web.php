f6d00457c549a523033d2c7a12404da3ead24c6a
Eiji-16 <ajph305@gmail.com>
2026-07-05 18:54:13 +0800

    Fix: Point root web routing to React view

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// This forces Laravel to look for your main React index view
+Route::get('{any}', function () {
+    return view('welcome'); // Or change 'welcome' to the specific name of your blade file if different
+})->where('any', '.*');
