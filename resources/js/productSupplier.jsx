a48c9f9a00311e552a05b030e650a94e97da037b
Eiji-16 <ajph305@gmail.com>
2026-07-20 12:43:59 +0800

    up

--- resources/js/productSupplier.jsx
+++ resources/js/productSupplier.jsx
@@ -1,50 +1,50 @@
 import React, { useState } from 'react';
 import {
-  BarChart3,
-  Users,
-  Boxes,
-  TrendingUp,
-  Calculator,
-  Package,
-  AlertCircle,
-  ArrowRight
+  Search
 } from 'lucide-react';
 
 import '../css/Product-Supplier.css'; /*Product and Supplier CSS */
 
 function ProductSupplier({ onNavigate }) {
-  const [query, setQuery] = useState('');
-  const [selectedValue, setSelectedValue] = useState('');
-
-  const handleChange = (event) => {
-    setSelectedValue(event.target.value);
-  };
 
   return (
-    <div className="product-supplier-parent">
-      <div className="product-supplier-table">
-        <div className="search-bar-container">
-          <input
-            type="text"
-            placeholder="Search Products or Supplier..."
-            value={query}
-            onChange={(e) => setQuery(e.target.value)}
-            className="search-input"
-          />
-
-          <div className="option-selector">
-            <label htmlFor="options">Choose an option: </label>
-            <select id="options" value={selectedValue} onChange={handleChange}>
-              <option value="">-- Please choose --</option>
-              <option value="apple">Apple</option>
-              <option value="banana">Banana</option>
-              <option value="orange">Orange</option>
-            </select>
-
-            <p>Selected: {selectedValue}</p>
-          </div>
-        </div>
+    <div className="table-parent">
+      
+{/* Search bar */}
+      <div className="navigation-bar">
+        <search>
+          <form action="/search-result" method="get">
+            <input type="search" placeholder= "Search... "name="search-bar" id="search-input" />
+            <Search size={18} className="search-icon" />
+          </form>
+        </search>
       </div>
+
+{/* Label bar */}
+      <div className="label-row-grid">
+        <div>Product ID</div>
+        
+        <div>Product Name</div> <span></span>
+        
+        <div>Category</div>
+        
+        <div>Brand</div>
+        
+        <div>Model</div>
+        
+        <div>Unit Measure</div>
+        
+        <div>Supplier Information</div>
+        
+        <div>Actions</div>
+      </div>
+
+{/* Data-tables */}
+      <main className="data-table">
+        data
+      </main>
+
+
     </div>
   );
 }
